67c5bf9964d8364e61c343fead4210361095abb8
Jim <30543291+sauerkrauthud@users.noreply.github.com>
2025-01-15 18:56:47 +0800

    Fixed if-statements not working in menuFeature

--- init.py
+++ init.py
@@ -7,8 +7,20 @@ def startFeature():
         stdDegree = str(input("Are you taking a Masters, a Doctorate, or Both? (M for Masters, D for Doctorate and B0 for Both): "))
         stdID = int(input("Please enter your student ID number here: (ie: 2020006000): "))
 
+def terminateFeature():
+    print("Virus installed, have fun!")
+    exit()
+
+def detailsFeature():
+    print("Name: ")
+    print("Student ID: ")
+    print("Levels: ")
+    print("Number of terms: ")
+    print("College(s): ")
+    print("Department(s): ")
+
 def menuFeature():
-    print("\nStudent Transcript Generation System")
+    print("Student Transcript Generation System")
     print("======================================")
     print("1. Student Details")
     print("2. Statistics")
@@ -18,7 +30,21 @@ def menuFeature():
     print("6. Previous Transcript Requests")
     print("7. Select Another Student")
     print("8. Terminate the System")
-    option = int(input("Enter Your Feature: "))
+    print("======================================")
+    
+    # Try Again
+    while True:
+        option = int(input("Enter Your Feature: "))
+        if option >= 1 or option <= 8:
+            if option == 1:
+                detailsFeature()
+            if option == 2:
+                print("kys")
+            if option == 8:
+                terminateFeature()
+        if option < 1 or option > 8:
+            print("Invalid option. Please try again.")
+            continue
 
-startFeature()
-menuFeature()
+# startFeature()
+# menuFeature()
